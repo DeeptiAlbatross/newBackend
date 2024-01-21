de41946fe8738e43a46c02bb2bf6f93c59c19b74
deepti <2021399693.deepti@ug.sharda.ac.in>
2024-01-21 16:41:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+var app = express();
+var cors = require('cors');
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+
+const url = "mongodb+srv://deepti23rajput:complicated123@cluster0.7lh26ez.mongodb.net/backendDb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected successfully to MongoDB");
+    // Continue with your code here
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+
+
+mongoose.connection.on("connect", (err) => {
+  console.log("Database is connected...");
+});
+
+// require("./routing")(app);
+// var express = require('express');
+// var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// listening port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`app is live at ${PORT}`);
+});
+
+
+
---
+++ routing.js
@@ -0,0 +1,7 @@
+
+// console.log("Connected");
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
